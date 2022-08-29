16234239ecc5bf94b077a90227755545856c5d0f
Joem Elias Sanez <israelias.js@gmail.com>
2022-08-29 22:43:06 +0800

    test: successful Java application connection to OrientDB

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,7 +5,7 @@ public class Application {
   public static void main(String[] args) {
 
     OrientDB orient = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig());
-    ODatabaseSession db = orient.open("test", "admin", "admin");
+    ODatabaseSession db = orient.open("orientdb-java-boilerplate", "admin", "admin");
 
    // Nothing in this session yet
 
